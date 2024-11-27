7b1f611c52ffdee3a8bd1a5d7e77a7728c45925a
sabalearning01 <sabatemitayo44@gmail.com>
2024-11-27 13:56:27 +0100

    push

--- src/Base/Apikit.jsx
+++ src/Base/Apikit.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 let Apikit = axios.create({
-    baseURL:'https://proodoosfiles.onrender.com/api',
+    baseURL:'https://proodoosfiles.onrender.com/',
     
     headers:{
         "Content-Type": "application/json",
